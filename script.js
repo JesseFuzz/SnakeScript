e38274bd5ea3b542d4800f935e0044b6ecd3f836
Jesse <jessesouzaall@gmail.com>
2022-06-13 22:54:48 -0300

    3 finalizada a parte de de criar como a cobrinha se movimentará

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 let canvas = document.getElementById("snake"); //estou ligando com o id do canvas no html
 let context = canvas.getContext("2d");
 let box = 32; 
+let direction = "right";
 let snake = [];
 snake[0] = {x:8*box, y:8*box} //esse array de snake vazio acima vai receber esse x e y na posição zero do array
 
@@ -11,10 +12,30 @@ function criarBG(){
 
 function criarCobrinha(){
     for(i=0; i<snake.length; i++){ 
-        context.fillStyle = "red";
+        context.fillStyle = "green";
         context.fillRect(snake[i].x, snake[i].y, box, box);
     }
 }
- 
-criarBG();
-criarCobrinha();
+
+function iniciarJogo(){
+    criarBG();
+    criarCobrinha();
+
+    let snakeX = snake[0].x;
+    let snakeY = snake[0].y;
+
+    if (direction=="right") snakeX = snakeX + box;
+    if (direction=="left") snakeX-=box;
+    if (direction=="up") snakeY = snakeY - box;
+    if (direction=="down") snakeY+=box;
+
+    snake.pop();
+
+    let cabeça = {
+        x: snakeX,
+        y: snakeY
+    }
+
+    snake.unshift(cabeça);
+}
+let jogo = setInterval(iniciarJogo, 100)
